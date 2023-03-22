f473dc9b3ddd665e360bfe04565096d54ac4d544
Breno Rosado <brenorosado.cjr@gmail.com>
2023-03-21 21:03:15 -0300

    delete car controller method

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -19,4 +19,16 @@ export class CarsController {
 
     return response.status(201).json(createdCar);
   };
+
+  async delete (request: Request, response: Response) {
+    const { requestingUser } = request.body;
+    const { id } = request.params;
+
+    const carsServices = new CarsService();
+    const carsRepository = new CarsRepository();
+
+    await carsServices.delete(requestingUser, id, carsRepository);
+
+    return response.status(200).json("Carro deletado com sucesso.");
+  };
 };
